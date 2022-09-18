711e5e3a2244653ae927b22999f4b3743432eed0
Bob Baker <bob@gmail.com>
2022-09-18 08:32:25 +0530

    lambda workshop added

---
+++ src/com/java8/lambda/demo/FunctionalInterfaceDemo.java
@@ -0,0 +1,10 @@
+package com.java8.lambda.demo;
+
+public interface FunctionalInterfaceDemo extends Runnable{
+	@Override
+	default void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/java8/lambda/demo/MyFunctionalInterface.java
@@ -0,0 +1,5 @@
+package com.java8.lambda.demo;
+
+public interface MyFunctionalInterface {
+
+}
